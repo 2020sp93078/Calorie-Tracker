eebc3ad8021f32429af6d819ad96376a2aac7953
singhsidak54 <singhsidak54@gmail.com>
2021-12-15 19:58:36 +0530

    added google sign in

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
-import 'package:calorie_tracker/splash_screen.dart';
+import 'package:calorie_tracker/screens/login_screen/login_screen.dart';
+import 'package:calorie_tracker/screens/splash_screen/splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -11,7 +16,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
---
+++ lib/screens/splash_screen/splash_screen.dart
@@ -0,0 +1,63 @@
+import 'package:calorie_tracker/screens/home_screen/home_screen.dart';
+import 'package:calorie_tracker/screens/login_screen/login_screen.dart';
+import 'package:calorie_tracker/screens/user_info_screen/user_info_screen.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    _authCheck();
+  }
+
+  void _authCheck() async {
+    FirebaseAuth.instance.userChanges().listen((user) async {
+      if (user == null) {
+        Get.offAll(() => const LoginScreen());
+      } else if (await _userDetailExists(user.uid)) {
+        Get.offAll(() => const HomeScreen());
+      } else {
+        Get.offAll(() => const UserInfoScreen());
+      }
+    });
+  }
+
+  Future<bool> _userDetailExists(String userId) async {
+    final snapShot =
+        await FirebaseFirestore.instance.collection('users').doc(userId).get();
+
+    return snapShot.exists;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Image.asset(
+          "assets/images/splash-screen.png",
+          fit: BoxFit.fill,
+          width: Get.width,
+        ),
+        Positioned(
+          top: Get.height * 0.1,
+          left: Get.width * 0.2,
+          child: Image.asset(
+            "assets/images/logo.png",
+            width: Get.width * 0.6,
+          ),
+        )
+      ],
+    );
+  }
+}
