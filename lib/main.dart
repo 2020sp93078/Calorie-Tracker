03e1a632b43afda8d84fd7d96d9bec66fd4979d7
Keshri <ritu.keshri@sap.com>
2021-12-15 20:26:20 +0530

    adding ios web support

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
-import 'package:calorie_tracker/screens/login_screen/login_screen.dart';
 import 'package:calorie_tracker/screens/splash_screen/splash_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+import 'firebase_options.dart';
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
